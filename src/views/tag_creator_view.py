3895d5335607bdb9045204bb3cb0114a901775a3
João Bebber <joao.gbebber@gmail.com>
2024-02-15 00:40:48 -0300

    feat: Implementing view for tag creator with http types

---
+++ src/views/tag_creator_view.py
@@ -0,0 +1,15 @@
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+
+class TagCreatorView:
+  '''
+    Responsability for interacting with HTTP
+  '''
+  def validate_and_create(self, http_request: HttpRequest):
+    body = http_request.body
+    product_code = body["product_code"]
+
+    print('Estou na minha view')
+    print(product_code)
+
+    return HttpResponse(status_code=200, body={"hello": "from here!"})
